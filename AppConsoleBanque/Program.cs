f13b19e745814c71fb9a39ac3f20dbf0c0f37aeb
ballec <allec.benjamin@gmail.com>
2017-04-06 23:57:45 +0200

    soluceBanque

---
+++ AppConsoleBanque/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CompteBancaire = AppConsoleBanque.CompteBancaire;
+
+namespace AppConsoleBanque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Création d'un objet CompteBancaire
+            CompteBancaire unCompte = new CompteBancaire();
+
+            //Affichage du contenu des propriétés
+            Console.WriteLine("Numéro du compte : " + unCompte.NumCompte);
+            Console.WriteLine("Titulaire du compte : " + unCompte.NomTitulaire);
+            Console.WriteLine("Solde du compte : " + unCompte.SoldeCompte);
+
+            //Test de la valeur titulaire
+            if (unCompte.NomTitulaire == "")
+                Console.WriteLine("Ce que contient le nom du titulaire : une chaîne vide !");
+
+            if (unCompte.NomTitulaire == null)
+                Console.WriteLine("Ce que contient le nom du titulaire : la valeur null !");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ libBanque/CompteBancaire.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libBanque
+{
+    public class CompteBancaire
+    {
+        #region Propriétés privées de la classe
+        //private properties
+        private string numCompte;
+        private string nomTitulaire;
+        private Decimal soldeCompte;
+        #endregion
+
+        #region property (accesseurs)
+        //property (accesseurs)
+        public string NumCompte
+        {
+            get { return numCompte; }
+            set { numCompte = value; }
+        }
+        public string NomTitulaire
+        {
+            get { return nomTitulaire; }
+            set { nomTitulaire = value; }
+        }
+        public Decimal SoldeCompte
+        {
+            get { return soldeCompte; }
+        }
+        #endregion
+
+        #region Constructeurs de la classe
+        /// <summary>
+        /// Initialise une  nouvelle instance de la classe libanque.CompteBancaire
+        /// </summary>
+        /// <param name="sonNumero">Le numéro du compte bancaire</param>
+        /// <param name="sonTitulaire">Le nom du titulaire du compte : nom + prénom</param>
+        /// <remarks>Le solde du compte sera initialisé à 0
+        public CompteBancaire(string sonNumero, string sonTitulaire)
+        {
+            numCompte = sonNumero;
+            nomTitulaire = sonTitulaire;
+            soldeCompte = 0;
+        }
+
+        public CompteBancaire(string sonNumero, string sonTitulaire, decimal sonSoldeInitial)
+        {
+            numCompte = sonNumero;
+            nomTitulaire = sonTitulaire;
+            soldeCompte = sonSoldeInitial;
+        }
+        #endregion
+    }
+}
